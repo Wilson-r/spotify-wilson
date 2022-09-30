4d02428877b3153ce04c89ca9b74b747f5760769
Wilson-r <wilson.riccardo@iisgalvanimi.edu.it>
2022-09-30 09:43:50 +0000

    ultimi ritocchi

--- first-routed-app-wilson/src/app/spotify.service.ts
+++ first-routed-app-wilson/src/app/spotify.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'  
 })
@@ -11,12 +11,22 @@ export class SpotifyService {
   searchTrack(query: string) {
     const url = `https://api.spotify.com/v1/search?q=${query}&type=track`;
     const headers = new HttpHeaders({
-      Authorization:
-        'Bearer BQD9qjxjQpOMtF_erZnSCQtfYDPl6jsA-nWLT63KMKez0ueaiKhrlFBlU_4SFt2ho42TxawdO6VZGrctETOm1n2PWes0nsjRleoVFgjPaMGvgQhDmateKzLZVRl6FGV-m_69z_v84C2qKAJ0SI-1dcxL_N8WsFYjRtZ-mKs0LUuLR2jguQdnoGsU-IgHx3Tsyy8'
+      Authorization: environment.oauthToken
     });
 
     let obsTracks = this.http.get(url, { headers });
     return obsTracks;
  
   }
+  getTrack(id: string) {
+    const url = `https://api.spotify.com/v1/tracks/${id}`;
+    const headers = new HttpHeaders({
+      Authorization:
+        'Bearer BQCtPRxN9eyTGOq2maLFtiJ7xmUW2v9e1G5XgtpY_xwRttKtAQthgb3T0ONnNHoQcmCETLxHlkAqvRTjNaIKem-UMIX2ICYgfFskDQ9EUt5VSN7cxLcO3Ex0twRfP0u2OugvxiQ4pHCVvpoTks5E_nhm_MOQ1NwEfjhGdHYzpa4O9gl6FcItMj3PCARy75rwlDo'
+    });
+    
+    return this.http.get(url, { headers });
+  }
 }
+
+ 
--- first-routed-app-wilson/src/environments/environment.ts
+++ first-routed-app-wilson/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  oauthToken: 'Bearer BQCtPRxN9eyTGOq2maLFtiJ7xmUW2v9e1G5XgtpY_xwRttKtAQthgb3T0ONnNHoQcmCETLxHlkAqvRTjNaIKem-UMIX2ICYgfFskDQ9EUt5VSN7cxLcO3Ex0twRfP0u2OugvxiQ4pHCVvpoTks5E_nhm_MOQ1NwEfjhGdHYzpa4O9gl6FcItMj3PCARy75rwlDo'
 };
 
 /*
